37ac11b8de8372761a73d991676a06a43be6c0e9
codefug <86544979+codefug@users.noreply.github.com>
2024-06-22 10:00:10 +0900

    test deploy 2 edit page

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Byssss{" "}
+            배포 확인 완료{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
